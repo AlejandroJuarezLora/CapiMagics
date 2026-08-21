6515675fdb89b05211d0d9eea331f7b3deb6f7f7
Gonzalo Sebastián Torres Chávez <70080846+Bondrewd117@users.noreply.github.com>
2026-08-20 18:36:54 -0600

    Movimiento de .gds .lef y .v

--- designs/libs/snn_analog/lif/neurona_lvs.v
+++ designs/libs/snn_analog/lif/neurona_lvs.v
@@ -1,6 +1,6 @@
 module neurona_lvs (
-    input  Vdd,   // alimentacion
-    input  Vss,   // tierra
+    inout  Vdd,   // alimentacion
+    inout  Vss,   // tierra
     input  Iin,   // corriente de entrada
     output spike,     // salida de disparo
     output spike_neg  // salida de disparo negada
