4e1663ceb6e687dc555ca09ab2b4e9620771233c
AlejandroLora <aljuarezl@ipn.mx>
2026-08-27 14:15:32 -0600

    first macro succesfully compiled :D

---
+++ designs/libs/snn_analog/macros/layer_input/runs/test/20-openroad-generatepdn/layer_input.nl.v
@@ -0,0 +1,47 @@
+module layer_input (Iext1,
+    Iext2,
+    Iext3,
+    Iext4,
+    nvout_1,
+    nvout_2,
+    nvout_3,
+    nvout_4,
+    vdd,
+    vout_1,
+    vout_2,
+    vout_3,
+    vout_4,
+    vss,
+    VSS,
+    VDD);
+ input Iext1;
+ input Iext2;
+ input Iext3;
+ input Iext4;
+ output nvout_1;
+ output nvout_2;
+ output nvout_3;
+ output nvout_4;
+ inout vdd;
+ output vout_1;
+ output vout_2;
+ output vout_3;
+ output vout_4;
+ inout vss;
+ inout VSS;
+ inout VDD;
+
+
+ neurona_lvs x1 (.spike(vout_1),
+    .spike_neg(nvout_1),
+    .Iin(Iext1));
+ neurona_lvs x2 (.spike(vout_2),
+    .spike_neg(nvout_2),
+    .Iin(Iext2));
+ neurona_lvs x3 (.spike(vout_3),
+    .spike_neg(nvout_3),
+    .Iin(Iext3));
+ neurona_lvs x4 (.spike(vout_4),
+    .spike_neg(nvout_4),
+    .Iin(Iext4));
+endmodule
