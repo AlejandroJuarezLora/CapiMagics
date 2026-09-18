60e904de6497ecb24ac81af9a6f4a56233174c58
AlejandroLora <aljuarezl@ipn.mx>
2026-09-18 15:43:21 -0600

    adding script to install libraries 3v3

---
+++ designs/libs/snn_analog/install_3v3_pdk_cells.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+# Versión exacta utilizada por el template de wafer.space
+PDK_COMMIT="f6eeac7dad085ffcc829ccfd721f7b4ce39edcf7"
+
+# PDK utilizado por el proyecto
+# PDK="gf180mcuD"
+
+# # Crear/usar la carpeta gf180mcu en el directorio actual
+# PDK_ROOT="$(pwd)/gf180mcu"
+
+echo "=========================================="
+echo " Clonando GF180MCU PDK"
+echo "=========================================="
+echo "PDK_ROOT   : $PDK_ROOT"
+echo "PDK        : $PDK"
+echo "PDK_COMMIT : $PDK_COMMIT"
+echo "=========================================="
+
+ciel enable "$PDK_COMMIT" \
+    --pdk-root "$PDK_ROOT" \
+    --pdk-family "$PDK" \
+    --include-librarieciels all
+
+echo ""
+echo "=========================================="
+echo " GF180MCU PDK instalado correctamente"
+echo "=========================================="
+echo "Ubicación:"
+echo "$PDK_ROOT"
